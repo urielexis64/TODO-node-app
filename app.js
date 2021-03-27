5fe189b80871c5b6f8c265cde21b51b9df5e056d
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-27 15:03:06 -0700

    switch case to show selected option

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 require("colors");
-const {inquirerMenu, pause} = require("./helpers/inquirer");
+
+const {inquirerMenu, pause, readInput} = require("./helpers/inquirer");
+const Task = require("./models/task");
+const Tasks = require("./models/tasks");
 
 const main = async () => {
 	console.clear();
 
 	let opt = "";
+	const tasks = new Tasks();
 	do {
 		opt = await inquirerMenu();
-		console.log({opt});
+		switch (opt) {
+			case "1":
+				const desc = await readInput("Enter a description: ");
+				tasks.createTask(desc);
+				break;
+			case "2":
+				console.log(tasks);
+			default:
+				break;
+		}
+
 		await pause();
 	} while (opt !== "0");
 };
